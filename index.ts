852fc935d8306dd221c6f297a4bf148e4017e8e4
Peter Kota <kotapeter@gmail.com>
2024-01-17 15:01:15 +0100

    init

--- index.ts
+++ index.ts
@@ -4,15 +4,20 @@ const prisma = new PrismaClient()
 
 async function main() {
   setInterval(async () => {
-    const user = await prisma.user.upsert({
-      where: {email: 'alice@prisma.io'},
-      update: {},
-      create: {
-        name: 'Alice',
-        email: 'alice@prisma.io',
-      },
-    })
-    console.log(user)
+    try {
+      const user = await prisma.user.upsert({
+        where: {email: 'alice@prisma.io'},
+        update: {},
+        create: {
+          name: 'Alice',
+          email: 'alice@prisma.io',
+        },
+      })
+      console.log(user)
+    } catch (e) {
+      console.log('failed to fetch ')
+      console.log(e)
+    }
   }, 2000)
 }
 
